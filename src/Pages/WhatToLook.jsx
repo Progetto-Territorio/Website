629dc38156cd3ad918ce7779514de90a2d46832e
DESKTOP-4OSLPPA\andri <andrix.braia@gmail.com>
2023-05-23 18:38:28 +0200

    Pagina categoria monumento + scss

--- src/Pages/WhatToLook.jsx
+++ src/Pages/WhatToLook.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import "../CSS/WhatToLook.scss";
 
 export default function WhatToLook() {
-  return <div>whatToLook</div>;
+  return (
+    <div className="container">
+      <h1>Monumenti</h1>
+      <div className="card-container">
+        <div className="test" />
+
+        <div className="test" />
+        <div className="test" />
+      </div>
+    </div>
+  );
 }
